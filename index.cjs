0d95654b5e8a9654a86d4b649bbf2035e4d24345
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-03-02 12:49:22 -0500

    initial logic

---
+++ index.cjs
@@ -0,0 +1,23 @@
+// ":" //#;exec /usr/bin/env node --input-type=module - "$@" < "$0"
+
+const {program} = require("commander");
+
+program
+    .name("njk")
+    .version("0.1.0", '-v, --version')
+    .description("A cli for nunjucks");
+
+program
+    .argument("<input>", "Source templates to transform. May be a file or directory.")
+    .argument("<output>", "Output file or directory")
+    .option("-d, --data <path>", "Path to a json or yaml file", (val, prev) => {
+        return prev.concat([val]);
+    }, []);
+
+program.parse(process.argv);
+
+import("./app.js").then(app => {
+    app.run(program.processedArgs[0], program.processedArgs[1], program.opts());
+}).catch(err => {
+    throw new Error(err);
+})
